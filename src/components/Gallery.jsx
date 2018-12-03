967b0b6a989508c846adcd1f1496456df2d0e0c4
Michael Donovan <mike@eggborne.com>
2018-12-03 10:23:35 -0800

    Add check for empty image URLs

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,16 +5,16 @@ function Gallery(props) {
   return (
     <div id='gallery'>
       {props.imageArray.map((imageUrl, i) => {
-        return (
-          <div className='post-display' key={i}>
-            <img className='post-image' src={imageUrl}></img>
-
-            <div className='post-caption'>
-              {props.captionArray[i]}
+        if (imageUrl.length) {
+          return (
+            <div className='post-display' key={i}>
+              <img className='post-image' src={imageUrl}></img>
+              <div className='post-caption'>
+                {props.captionArray[i]}
+              </div>
             </div>
-            
-          </div>
-        );
+          );
+        }
       })}
     </div>
   );
