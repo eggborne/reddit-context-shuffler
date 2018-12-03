be70e5f784ee716c99d3578ecea065a7cde13778
Michael Donovan <mike@eggborne.com>
2018-12-03 07:59:06 -0800

    Add randomInt function

--- src/scripts/util.js
+++ src/scripts/util.js
@@ -3,11 +3,15 @@ export function shuffleArray(arr) {
   let temp;
   let index;
   while (ctr > 0) {
-      index = Math.floor(Math.random() * ctr);
-      ctr--;
-      temp = arr[ctr];
-      arr[ctr] = arr[index];
-      arr[index] = temp;
+    index = Math.floor(Math.random() * ctr);
+    ctr--;
+    temp = arr[ctr];
+    arr[ctr] = arr[index];
+    arr[index] = temp;
   }
   return arr;
+}
+
+export function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
